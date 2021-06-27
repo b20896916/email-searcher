a4214815dddbbde2c12741ee0e49f8787382112c
double112233 <doublewang910627@gmail.com>
2021-06-27 15:25:45 +0800

    Update trie.c

--- trie.c
+++ trie.c
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct _trie{
+    struct _trie *p[36];
+    int end;
+} trie;
+
 void insert(trie *o, char *s, int n, int k); // insert a string with length n and beginning at s to a trie
 int trie_search(trie *o, char *s, int n); // search whether a string with length n and beginning at s in a trie
 
